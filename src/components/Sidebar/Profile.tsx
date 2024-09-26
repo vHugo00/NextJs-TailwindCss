17100ba6268c688a3fd7dadd29046010e855598e
unknown <vv001226@gmail.com>
2024-09-25 21:29:26 -0300

    project completed

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,27 @@
+import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
+
+export interface ProfileProps { }
+
+export function Profile() {
+  return (
+    <div className="flex items-center gap-3">
+      <img
+        src="https://github.com/Vitor3g.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-col">
+        <span className="block text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Vitor Hugo
+        </span>
+        <span className="block text-sm text-zinc-500 dark:text-zinc-400">
+          Vitor.schell.f@gmail.com
+        </span>
+      </div>
+      <Button variant="ghost" className="ml-auto">
+        <LogOut className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      </Button>
+    </div>
+  )
+}
